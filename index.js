ac02a0ea8ddc8c0184e1a6df8e911505c38262ef
michaelwachira707@gmail.com <michaelwachira707@gmail.com>
2025-03-12 05:56:49 +0300

    adds changes to index.js

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your solution in this file!
+let employee = {
+    name: "the poet",
+    streetAddress: "123 choche"
+  };
+  
+  function updateEmployeeWithKeyAndValue(employee, key, value) {
+    return { ...employee, [key]: value };
+  }
+  
+  function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+  }
+  
+  function deleteFromEmployeeByKey(employee, key) {
+    const { [key]: deletedKey, ...rest } = employee;
+    return rest;
+  }
+  
+  function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+  }
